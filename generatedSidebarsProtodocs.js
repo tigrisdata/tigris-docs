11dbbef1cd5093e90bad6ff8291e089603466a43
Himank Chaudhary <himank@tigrisdata.com>
2022-04-28 11:07:40 -0700

    fix: pull in latest changes, fix docs repo

--- generatedSidebarsProtodocs.js
+++ generatedSidebarsProtodocs.js
@@ -6,8 +6,8 @@ module.exports = {
       items: [
         {
           type: "category",
-          label: "/server/v1",
-          items: [{ type: "doc", id: "server/v1/api.proto" }],
+          label: "tigrisdata.tigris.v1",
+          items: [{ type: "doc", id: "server/v1/api.proto", label: "Tigris" }],
         },
       ],
     },
